b7794ebddd38a0f74dd32ddb08f09dfec9f5207f
Anugrahas2001 <anugrahas2001@gmail.com>
2024-09-01 15:40:24 +0530

    patch method added to the cors

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const express = require("express");
 const { dataSource } = require("./db/connection.js");
 const { userRouter } = require("./router/userRouter.js");
 const { restuarentRouter } = require("./router/restuarentRouter.js");
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-const cors = require('cors');
+const cors = require("cors");
 dotenv.config();
 PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cors({
-//   origin: 'http://localhost:5173'
-// }));
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.use('/user', userRouter); 
+app.use("/user", userRouter);
 app.use("/restaurant", restuarentRouter);
 
 app.listen(PORT, () => {
