39b9f99ff352ebab807a71903d86d177eeb75532
Anugrahas2001 <anugrahas2001@gmail.com>
2024-09-02 22:36:16 +0530

    something changed

--- controller/restuarentController.js
+++ controller/restuarentController.js
@@ -210,7 +210,8 @@ async function updateRestuarent(req, res) {
     let imageUrl = restaurant.restaurantImg;
     if (req.file) {
       const result = await cloudinary.uploader.upload(req.file.path);
-      imageUrl=result.url.replace(/^http:/,"https");
+      // imageUrl=result.url.replace(/^http:/,"https");
+      imageUrl=result.url;
     }
 
     // const currentDate = new Date().toISOString().split("T")[0];
