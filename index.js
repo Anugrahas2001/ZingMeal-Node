cea38e127709ae06dad8bca06b2087668ed32b77
Anugrahas2001 <anugrahas2001@gmail.com>
2024-09-29 11:24:14 +0530

    deployment url is added

--- index.js
+++ index.js
@@ -14,9 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://anugraha--soft-pothos-8a37b8.netlify.app",
-  "https://soft-pothos-8a37b8.netlify.app",
-  "https://zing-meal-app-site-anugraha-ss-projects.vercel.app"
+  "https://66f8e2cf7f440000089c3a9e--soft-pothos-8a37b8.netlify.app/",
+  "https://zing-meal-app-git-anugraha-anugraha-ss-projects.vercel.app/"
 ];
 
 app.use(
